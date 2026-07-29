ce3603f25b06ea07aed7b1e8a6fa058b9d40acfe
Dilshan Geeth <ITBIN-2313-0032@horizoncampus.edu.lk>
2026-07-29 20:54:25 +0530

    Implement image resizing function

--- preprocessing/image_resize.py
+++ preprocessing/image_resize.py
@@ -3,4 +3,11 @@ import os
 
 IMAGE_SIZE = (224, 224)
 
+def resize_image(image_path):
+    image = cv2.imread(image_path)
+    if image is None:
+        return None
+    return cv2.resize(image, IMAGE_SIZE)
+
+
 print("Image resize preprocessing script")
