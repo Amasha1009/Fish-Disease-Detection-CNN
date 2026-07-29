3549ca74b7c05eb8262233f5276021f61efae795
Dilshan Geeth <ITBIN-2313-0032@horizoncampus.edu.lk>
2026-07-29 20:55:28 +0530

    Add initial CNN model templates

--- models/baseline_cnn.py
+++ models/baseline_cnn.py
@@ -3,3 +3,7 @@ Baseline CNN model for aquarium fish disease detection.
 
 Implementation will be completed in later milestones.
 """
+
+import tensorflow as tf
+
+print("Baseline CNN model initialized.")
