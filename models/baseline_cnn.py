cb4efb04ec6f5ef49b5dd5d21f101bcf8a8e53d2
Dilshan Geeth <ITBIN-2313-0032@horizoncampus.edu.lk>
2026-07-29 20:24:15 +0530

    Initial project structure

---
+++ models/baseline_cnn.py
@@ -0,0 +1,5 @@
+"""
+Baseline CNN model for aquarium fish disease detection.
+
+Implementation will be completed in later milestones.
+"""
---
+++ preprocessing/image_resize.py
@@ -0,0 +1,6 @@
+import cv2
+import os
+
+IMAGE_SIZE = (224, 224)
+
+print("Image resize preprocessing script")
